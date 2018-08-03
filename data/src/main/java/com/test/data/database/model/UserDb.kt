d3cda15bfed2f724cb7bd9c7f92aa53e3024ae59
portatilcasa <daniavialdomingo@gmail.com>
2018-08-03 16:56:05 +0200

    refactor

--- data/src/main/java/com/test/data/database/model/UserDb.kt
+++ data/src/main/java/com/test/data/database/model/UserDb.kt
@@ -9,45 +9,46 @@ import com.test.data.database.model.UserDb.Companion.TABLE_NAME
 @Entity(tableName = TABLE_NAME,
         indices = [(Index("id"))])
 
-class UserDb {
+data class UserDb(
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        val id: Int,
+        @ColumnInfo(name = "login")
+        val login: String,
+        @ColumnInfo(name = "node_id")
+        val nodeId: String,
+        @ColumnInfo(name = "avatar_url")
+        val avatarUrl: String,
+        @ColumnInfo(name = "gravatar_id")
+        val gravatarId: String,
+        @ColumnInfo(name = "url")
+        val url: String,
+        @ColumnInfo(name = "html_url")
+        val htmlUrl: String,
+        @ColumnInfo(name = "followers_url")
+        val followersUrl: String,
+        @ColumnInfo(name = "following_url")
+        val followingUrl: String,
+        @ColumnInfo(name = "gists_url")
+        val gistsUrl: String,
+        @ColumnInfo(name = "starred_url")
+        val starredUrl: String,
+        @ColumnInfo(name = "subscriptions_url")
+        val subscriptionsUrl: String,
+        @ColumnInfo(name = "organizations_url")
+        val organizationsUrl: String,
+        @ColumnInfo(name = "repos_url")
+        val reposUrl: String,
+        @ColumnInfo(name = "events_url")
+        val eventsUrl: String,
+        @ColumnInfo(name = "received_events_url")
+        val receivedEventsUrl: String,
+        @ColumnInfo(name = "type")
+        val type: String,
+        @ColumnInfo(name = "site_admin")
+        val siteAdmin: Boolean) {
+
     companion object {
         const val TABLE_NAME = "users"
     }
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    var id: Int = 0
-    @ColumnInfo(name = "login")
-    var login: String = ""
-    @ColumnInfo(name = "node_id")
-    var nodeId: String = ""
-    @ColumnInfo(name = "avatar_url")
-    var avatarUrl: String = ""
-    @ColumnInfo(name = "gravatar_id")
-    var gravatarId: String = ""
-    @ColumnInfo(name = "url")
-    var url: String = ""
-    @ColumnInfo(name = "html_url")
-    var htmlUrl: String = ""
-    @ColumnInfo(name = "followers_url")
-    var followersUrl: String = ""
-    @ColumnInfo(name = "following_url")
-    var followingUrl: String = ""
-    @ColumnInfo(name = "gists_url")
-    var gistsUrl: String = ""
-    @ColumnInfo(name = "starred_url")
-    var starredUrl: String = ""
-    @ColumnInfo(name = "subscriptions_url")
-    var subscriptionsUrl: String = ""
-    @ColumnInfo(name = "organizations_url")
-    var organizationsUrl: String = ""
-    @ColumnInfo(name = "repos_url")
-    var reposUrl: String = ""
-    @ColumnInfo(name = "events_url")
-    var eventsUrl: String = ""
-    @ColumnInfo(name = "received_events_url")
-    var receivedEventsUrl: String = ""
-    @ColumnInfo(name = "type")
-    var type: String = ""
-    @ColumnInfo(name = "site_admin")
-    var siteAdmin: Boolean = false
 }
--- data/src/main/java/com/test/data/database/model/mapper/DataBaseMapper.kt
+++ data/src/main/java/com/test/data/database/model/mapper/DataBaseMapper.kt
@@ -4,29 +4,12 @@ import com.test.data.database.model.UserDb
 import com.test.domain.model.User
 import com.test.domain.model.mapper.Mapper
 
-class DataBaseMapper: Mapper<UserDb, User>() {
+class DataBaseMapper : Mapper<UserDb, User>() {
     override fun map(model: UserDb): User = model.run {
         User(login, id, nodeId, avatarUrl, gravatarId, url, htmlUrl, followersUrl, followingUrl, gistsUrl, starredUrl, subscriptionsUrl, organizationsUrl, reposUrl, eventsUrl, receivedEventsUrl, type, siteAdmin)
     }
 
-    override fun inverseMap(model: User): UserDb = UserDb().apply {
-        login = model.login
-        id = model.id
-        nodeId = model.nodeId
-        avatarUrl = model.avatarUrl
-        gravatarId = model.gravatarId
-        url = model.url
-        htmlUrl = model.htmlUrl
-        followersUrl = model.followersUrl
-        followingUrl = model.followingUrl
-        gistsUrl = model.gistsUrl
-        starredUrl = model.starredUrl
-        subscriptionsUrl = model.subscriptionsUrl
-        organizationsUrl = model.organizationsUrl
-        reposUrl = model.reposUrl
-        eventsUrl = model.eventsUrl
-        receivedEventsUrl = model.receivedEventsUrl
-        type = model.type
-        siteAdmin = model.siteAdmin
+    override fun inverseMap(model: User): UserDb = model.run {
+        UserDb(id, login, nodeId, avatarUrl, gravatarId, url, htmlUrl, followersUrl, followingUrl, gistsUrl, starredUrl, subscriptionsUrl, organizationsUrl, reposUrl, eventsUrl, receivedEventsUrl, type, siteAdmin)
     }
 }
