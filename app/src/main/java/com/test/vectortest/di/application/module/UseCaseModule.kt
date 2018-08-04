6225467d8ad1b838958f265818b8bbb2bc822b03
portatilcasa <daniavialdomingo@gmail.com>
2018-08-04 12:05:07 +0200

    update mapper

--- app/src/main/java/com/test/vectortest/di/application/module/UseCaseModule.kt
+++ app/src/main/java/com/test/vectortest/di/application/module/UseCaseModule.kt
@@ -2,6 +2,8 @@ package com.test.vectortest.di.application.module
 
 import com.test.domain.interactors.GetCachedUserSingleUserCase
 import com.test.domain.interactors.GetUsersSingleUseCase
+import com.test.domain.interactors.type.SingleUseCaseWithParameter
+import com.test.domain.model.User
 import com.test.domain.repository.IRepository
 import dagger.Module
 import dagger.Provides
@@ -11,9 +13,9 @@ import javax.inject.Singleton
 class UseCaseModule {
     @Provides
     @Singleton
-    fun provideGetUsersSingleUseCase(repository: IRepository): GetUsersSingleUseCase = GetUsersSingleUseCase(repository)
+    fun provideGetUsersSingleUseCase(repository: IRepository): SingleUseCaseWithParameter<Int, List<User>> = GetUsersSingleUseCase(repository)
 
     @Provides
     @Singleton
-    fun provideGetCachedUsersSingleUseCase(repository: IRepository): GetCachedUserSingleUserCase = GetCachedUserSingleUserCase(repository)
+    fun provideGetCachedUsersSingleUseCase(repository: IRepository): SingleUseCaseWithParameter<Int, List<User>> = GetCachedUserSingleUserCase(repository)
 }
